8d97a47203f866c20a4ad1be9d12a7dc514ab788
kmaza <kemazam@ists.edu.ec>
2025-06-09 17:26:30 -0500

    pruebas secundarias

---
+++ prueba.py
@@ -0,0 +1,10 @@
+'''segunda prueba, ahora se inicializo con el comando git flow init
+lo cual me permite iniciar con un flujo de trabajo y crear diversas ramas'''
+
+print("Prueba de funcion")
+
+contador = 0
+
+while contador < 100:
+    print("yo soy biker")
+    contador += 1
