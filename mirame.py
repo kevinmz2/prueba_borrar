0f56b85506874dadcefae83a8476095154db721b
kmaza <kemazam@ists.edu.ec>
2025-06-11 15:35:06 -0500

    nueva funcion

---
+++ mirame.py
@@ -0,0 +1,10 @@
+print('Creando una nueva funcion')
+
+class funcion_gato():
+    def __init__(self):
+        self.nombre = 'Roberto'
+        self.edad = 5
+        self.color = 'negro'
+        self.animal = 'gato'
+        self.peso = 12
+funcion_gato()
