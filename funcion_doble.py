08aee2dab13f4787858b53a320f2ed0174882319
kmaza <kemazam@ists.edu.ec>
2025-06-11 15:38:47 -0500

    funcion doble

---
+++ funcion_doble.py
@@ -0,0 +1,8 @@
+print('Creando una nueva funcion')
+
+def funcion():
+    numero1 = float(input('Ingrese un numero: '))
+    numero2 = float(input('Ingrese otro numero: '))
+    print(f'El resultado es: {numero1 + numero2}')
+
+funcion()
