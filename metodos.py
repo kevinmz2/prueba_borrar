b7557257a04b692c33b5a8ad8e3acab65d8b4a65
kmaza <kemazam@ists.edu.ec>
2025-06-11 16:08:52 -0500

    metodos de gato

---
+++ metodos.py
@@ -0,0 +1,9 @@
+print('creando un nuevo metodo')
+
+class gato():
+    def __init__(self):
+        self.nombre = 'Roberto'
+        self.edad = 5
+        self.color = 'negro'
+        self.animal = 'gato'
+        self.peso = 12
